df3fda80f733fdf4d3e8010470cfb655862eb8cf
shanti <shantisingh22@navgurukul.org>
2024-05-12 16:24:20 +0530

    completed the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import UserProfile from './component/card';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/card.js
@@ -0,0 +1,36 @@
+// src/UserProfile.js
+import React, { useState, useEffect } from 'react';
+import './UserProfile.css';
+
+function UserProfile() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?page=1&results=1&seed=abc')
+      .then(response => response.json())
+      .then(data => {
+        setUserData(data.results[0]);
+      })
+      .catch(error => console.error('Error fetching user data:', error));
+  }, []);
+
+  return (
+    <div className="card">
+      {userData && (
+        <>
+          <div className="avatar">
+            <img src={userData.picture.large} alt="User Profile" />
+          </div>
+          <div className="details">
+            <h2>{`${userData.name.first} ${userData.name.last}`}</h2>
+            <p>Email: {userData.email}</p>
+            <p>Phone: {userData.phone}</p>
+            <p>Location: {`${userData.location.city}, ${userData.location.country}`}</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default UserProfile;
